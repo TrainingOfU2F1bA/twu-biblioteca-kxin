bbca8a01dd56f4d4578cd514b713edef289844d8
saul <eternx@163.com>
2018-07-16 14:55:58 +0800

    <003> add(com.twu.bibloteca.model:create book model)

--- src/com/twu/biblioteca/model/Book.java
+++ src/com/twu/biblioteca/model/Book.java
@@ -1,4 +1,51 @@
 package com.twu.biblioteca.model;
 
+import java.util.Date;
+
 public class Book {
+    private String bookName;
+    private String authorName;
+    private Date publishDate;
+    private BookStatus status;
+
+    public Book() {
+    }
+
+    public Book(String bookName, String authorName, Date publishDate) {
+        this.bookName = bookName;
+        this.authorName = authorName;
+        this.publishDate = publishDate;
+    }
+
+    public BookStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(BookStatus status) {
+        this.status = status;
+    }
+
+    public String getBookName() {
+        return bookName;
+    }
+
+    public void setBookName(String bookName) {
+        this.bookName = bookName;
+    }
+
+    public String getAuthorName() {
+        return authorName;
+    }
+
+    public void setAuthorName(String authorName) {
+        this.authorName = authorName;
+    }
+
+    public Date getPublishDate() {
+        return publishDate;
+    }
+
+    public void setPublishDate(Date publishDate) {
+        this.publishDate = publishDate;
+    }
 }
