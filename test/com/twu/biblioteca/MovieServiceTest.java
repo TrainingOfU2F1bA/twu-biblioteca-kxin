ca8702fefae4e019c17ce0c43ebe548ec5c192a7
saul <eternx@163.com>
2018-07-17 14:14:30 +0800

    fix(MovieServiceTest):fix some bug!

--- test/com/twu/biblioteca/MovieServiceTest.java
+++ test/com/twu/biblioteca/MovieServiceTest.java
@@ -1,23 +1,37 @@
 package com.twu.biblioteca;
 
+import com.twu.biblioteca.model.Movie;
+import com.twu.biblioteca.repositories.MovieRepository;
 import com.twu.biblioteca.repositories.MovieRepositoryImpl;
 import com.twu.biblioteca.services.MovieService;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mockito;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
 
 public class MovieServiceTest {
 
     private MovieService service;
     private ByteArrayOutputStream out;
+    private List<Movie> movies;
+    private MovieRepository movieRepository;
 
     @Before
     public void setUp() {
         service = new MovieService();
-        service.setMovieRepository(new MovieRepositoryImpl());
+        movieRepository = Mockito.mock(MovieRepository.class);
+        movies = Arrays.asList(
+                new Movie(1l, "Titan X", "James Chou", "A", new Date(1996, 1, 1)),
+                new Movie(2l, "TiTan V", "James Chou", "A", new Date(2006, 1, 1))
+        );
+        Mockito.when(movieRepository.list()).thenReturn(movies);
+        service.setMovieRepository(movieRepository);
         out = new ByteArrayOutputStream();
         System.setOut(new PrintStream(out));
     }
@@ -32,8 +46,10 @@ public class MovieServiceTest {
 
     @Test
     public void testCheckOut() {
+        Mockito.when(movieRepository.find(1l)).thenReturn(movies.get(0));
         service.checkoutMovie(1l);
         Assert.assertTrue(out.toString().endsWith("Just Enjoy the Movie at Home!\r\n"));
+        Mockito.when(movieRepository.find(-8l)).thenReturn(null);
         service.checkoutMovie(-8l);
         Assert.assertTrue(out.toString().endsWith("Invalid Movie ID!\r\n"));
     }
